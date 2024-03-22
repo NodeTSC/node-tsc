e59b3bbbf253c66bd403807d7e299053740097d8
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2024-03-22 11:10:38 +0700

    update viz data for knn

--- node/knn_node.py
+++ node/knn_node.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from node import ClassifierNodeImpl
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+import pandas as pd
 from .shapelet_transform_node import ShapeletTransformNode
 
 
@@ -15,6 +16,15 @@ class KnnNode(ClassifierNodeImpl):
         self.classifier = KNeighborsClassifier(**kwargs)
         
     def get_visualize_data(self) -> dict[str, Any]:
+        data: pd.DataFrame = self.data.get_output("data")
+        target_label = self.data.get_output("meta")["target"]
+        
+        predicted_label = self.classifier.predict(data.drop(columns=target_label))
         return {
+            "knn": {
+                "timeseries": data.drop(columns=target_label).values.tolist(),
+                "labels": list(data[target_label]),
+                "predicts": predicted_label,
+            },
             "scores": self.scores
         }
